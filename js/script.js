08279b28557eb145ae8b2d231e7ff0d347e27f3c
Tyler Stambaugh <11870675+tylerstambaugh@users.noreply.github.com>
2022-10-27 19:18:09 -0400

    got carousel working manually

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-$(function() {
+$(function () {
     //set options
     var speed = 500; //fade speed
     var autoswitch = true; //auto slider option
     var autoswitch_speed = 4000; //delay for autoswitching active slide
+    var isLast = false;
 
     //add initial active class to first slide
     $('.slide').first().addClass('active');
@@ -13,22 +14,35 @@ $(function() {
     //show active slide
     $('.active').show();
 
-    $('#next').on('click', function(){
-       $('.active').removeClass('active').addClass('oldActive');
-       
-       if($('oldActive').is(':last-child')){
+
+    $('#next').on('click', function () {
+        $('.active').removeClass('active').addClass('oldActive');
+
+        if ($('.oldActive').is(':last-child')) {
             $('.slide').first().addClass('active');
-       } else {
-        $('.oldActive').next().addClass('active');
-       }
+        } else {
+            $('.oldActive').next().addClass('active');
+        }
 
-       $('.oldActive').removeClass('oldActive');
-       $('.slide').fadeOut(speed);
-       $('.active').fadeIn(speed);
+        $('.oldActive').removeClass('oldActive');
+        $('.slide').fadeOut(speed);
+        $('.active').fadeIn(speed);
     });
 
-    $('#prev').on('click', function(){
+    $('#prev').on('click', function () {
+        $('.active').removeClass('active').addClass('oldActive');
 
+        if ($('.oldActive').is(':first-child')) {
+            $('.slide').last().addClass('active');
+        } else {
+            $('.oldActive').prev().addClass('active');
+        }
+
+        $('.oldActive').removeClass('oldActive');
+        $('.slide').fadeOut(speed);
+        $('.active').fadeIn(speed);
     });
+
+    
 });
 
